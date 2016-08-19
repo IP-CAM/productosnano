68995155028b89a726588064f612d6f351d75bb9
mbolanosc <mrybc96@gmail.com>
2016-08-19 15:10:08 -0600

    mas del local storage

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/**
+ * [when everything its ready do: hide all the divs from the html, get the local storage
+ * set in the plus and minus functions, do a log in to be avail to use the api things]
+ */
 jQuery(document).ready(function(){
     var hideEverything = function(){
         $("#divTabletas").hide();
@@ -7,21 +11,21 @@ jQuery(document).ready(function(){
     }
     hideEverything();
     var getLocalStorage = function(){
-        //get the local storage
+        /*get the local storage
+        var newAceitesValues = localStorage.getItem("divAceitesLocalStorage");
+        console.log('valores traidos de local storage: '+ newAceitesValues);
 
-
-        //var jazDiezLs = localStorage.getItem("jazDiez");
-        //console.log('datos del local storage: '+ jazDiezLs);
-        //set the local storage
-        //document.getElementById("Jazmin10input").innerHTML = jazDiezLs;
-        //document.getElementById("Jazmin10input").value = String(jazDiezLs);
-        //var getlocal = localStorage.getItem("precios");
-        //parslocal = JSON.parse(getlocal)
-        //localStorage.setItem("precios", JSON.stringify(nuevovalor));
+        show the local storage in the inputs
+        var divAceitesArray = $('input[value="0"]').map(function(){
+             return this.value;
+        }).get();*/
+        var newArrayValues = localStorage.getItem("InputsValues");
+      
+       
+        
+        
     }
-    getLocalStorage();
-
-    /*log in*/
+    //getLocalStorage();
     var loginData={username:'test',password:'shto33'}
 
    jQuery.ajax({
@@ -38,14 +42,20 @@ jQuery(document).ready(function(){
         }
     });
 });
-//variables globales
+/** 
+ * @type {Number}
+ */
 var globalQuantity = 0;
 var globalTotalMoney = 0;
 var globalSubTotal = 0;
 var globalImpuesto = 0;
+var dataLocalStorage = [];
 
 
-
+/**
+ * [hideEverythingTwice hide everything outside the document ready]
+ * @return {[none]} []
+ */
 var hideEverythingTwice=function(){
     $("#divTabletas").hide();
     $("#divAerosoles").hide();
@@ -53,14 +63,21 @@ var hideEverythingTwice=function(){
     $("#divAceites").hide();
     $("#divResumen").hide();
 }
-
+/**
+ * [incrementInput increment the input of the label buttons]
+ * @param  {[id]} idField [id from the input field to increment]
+ * @return {[none]}       
+ */
 var incrementInput = function(idField){
     var actualNumber = parseInt(document.getElementById(idField).value);
     var newValue = actualNumber  + 1;
     document.getElementById(idField).value = String(newValue);
-   
 }
-
+/**
+ * [decrementInput decrement the input of the label buttons and validate]
+ * @param  {[id]} idField [id from the input field to decrement]
+ * @return {[none]}        
+ */
 var decrementInput = function(idField){
     var actualNumber = parseInt(document.getElementById(idField).value);
     if(actualNumber > 0){
@@ -68,22 +85,34 @@ var decrementInput = function(idField){
         document.getElementById(idField).value = String(newValue);
     };
 }
-
+/**
+ * [plusClick everytime you click the + btn get the values to do a post request and set the local storage]
+ * @param  {[id]} inputId  [id from the input field]
+ * @param  {[number]} aroma   
+ * @param  {[id]} producId
+ * @param  {[id]} aromaId  
+ * @return {[none]}
+ */
 function plusClick(inputId,aroma,producId,idField,aromaId){
     incrementInput(idField);
     console.log('inputId: ',inputId);
     console.log('cantidad producto: ',document.getElementById(idField).value);
 
     var productData=`{
-            "product_id":"`+producId+`",
-            "quantity": "1",
-            "option[`+aromaId+`]":"`+aroma+`"
-        }`;
-
+        "product_id":"`+producId+`",
+        "quantity": "1",
+        "option[`+aromaId+`]":"`+aroma+`"
+    }`;
+    var productLsObject=`{
+        "idField":"`+idField+`",
+        "value": "`+document.getElementById(idField).value+`"
+    }`;
+    //data api
     console.log(productData);
     productApi = JSON.parse(productData);
+    //data local storage
+    console.log('data local storage: ',productLsObject);
 
-    
     jQuery.ajax({
         url: 'http://www.productosnano.com/index.php?route=api/cart/add',
         type: 'POST',
@@ -93,23 +122,11 @@ function plusClick(inputId,aroma,producId,idField,aromaId){
             console.log('success aadding product to cart by input');
             console.log(data);
             getResume();
-            /*var jazDiezMl = document.getElementById("Jazmin10input").value;
-            localStorage.setItem("jazDiez", JSON.stringify(jazDiezMl));
-            var inputsAceites = $("#divAceites").map(function(){
-                    return $(this).val();
-                }).get();
-            console.log('inputs: ',inputsAceites);*/
-            var inputsAceites = document.getElementById("divAceites").getElementsByTagName("input").getAttribute('value');
-            console.log(inputsAceites);
+            dataLocalStorage.push(productLsObject);
+            //console.log('array para local storage: ' + dataLocalStorage);
+            //set the local storage
+            localStorage.setItem("InputsValues",(dataLocalStorage));
 
-    
-
-
-
-         
-
-
-            //localStorage.setItem("inputsAceites", JSON.stringify(jazDiezMl));
         },
         error: function(error){
             console.log('error adding product by input: ',error);
@@ -117,7 +134,12 @@ function plusClick(inputId,aroma,producId,idField,aromaId){
     });
 }
  
-
+/**
+ * [minusClick everytime you click the - btn get the values to do a post request to the api and edit the local storage]
+ * @param  {[id]} inputId    [id from the input field]
+ * @param  {[id]} idField    
+ * @param  {[string]} productKey [key from the product for edit the cart]
+ */
 function minusClick(inputId,idField,productKey){
     decrementInput(idField);
     console.log('inputId: ',inputId);
@@ -146,7 +168,10 @@ function minusClick(inputId,idField,productKey){
             }
     });
 }
-
+/**
+ * [getResume takes de data from the api and display it to the label front end
+ * this one does a get request bc its asking for the data cart]
+ */
 function getResume(){
     tbobyMoney = document.querySelector('#tbl-resumen tbody');
     tbobyProducts = document.querySelector('#tbl-resumen-products tbody');
@@ -223,13 +248,12 @@ function getResume(){
     return globalTotalMoney; 
 }    
 
-
+/**
+ * [this ones hide and show for the siguiente and atras buttons]
+ */
 $("#next-aceites").click(function(){
     hideEverythingTwice();
     $("#divTabletas").show();
-    
-    /*var inputDiezJazmin = document.getElementById("Jazmin10input").value;
-    localStorage.setItem("inputDiezJazmin", inputDiezJazmin);*/
 });
 $("#next-tabletas").click(function(){
     hideEverythingTwice();
@@ -260,9 +284,7 @@ $("#back-resumen").click(function(){
     $( "#divDifusores" ).show();
 }); 
 
-//pendiente: local storage, botones fuera del scroll.
-//hecho: cantidad en boton de carrito. Precio. resumen tabla 1 y 2.
-//urge: localstorage por cada click de mas o menos.
+
 
 
 
